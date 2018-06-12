7463b083e6bdc20af31830aeed428cfe6828f730
Stephen <wizebin@gmail.com>
2018-06-12 00:50:11 -0400

    Added constructor information to getShardedNexustate functions

--- src/ShardedNexustate.d.ts
+++ src/ShardedNexustate.d.ts
@@ -1,8 +1,15 @@
 import Nexustate from './nexustate';
 
+type nexustateOptions = {
+  saveCallback: (string, any) => any,
+  loadCallback: (string) => any,
+  storageKey: string,
+  noPErsist: boolean
+};
+
 declare class ShardedNexustate {
   getShard(string): Nexustate
-  createShard(string): Nexustate
+  createShard(string, nexustateOptions): Nexustate
   setAllPersistenceFunctions(saveCallback, loadCallback): void
 }
 
